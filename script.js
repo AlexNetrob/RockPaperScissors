8e55e28b86277f57ad1dfca3013fd0a2fead350f
Aleksander <bortenalex@gmail.com>
2022-08-16 22:57:57 +0100

    add computer random resoult function

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function getComputerChoice() { //generates random resoult for computer 
+    let random = Math.floor(Math.random() * 3) + 1;
+    
+    if (random === 1) {
+        return "rock";
+    } else if (random === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
