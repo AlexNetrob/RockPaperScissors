7610a5a983194465b1c6fccf6a22d78cde3fa920
Aleksander <bortenalex@gmail.com>
2022-08-16 22:58:46 +0100

    add function that plays the single round

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ function getComputerChoice() { //generates random resoult for computer
         return "scissors";
     }
 }
+
+function playRound (playerChoice = prompt("rock, paper, scissors?"), computer = getComputerChoice()) {
+    
+    lowercaseRes = playerChoice.toLowerCase();
+
+    console.log("You play: " + lowercaseRes);
+    console.log("computer plays: " + computer);
+
+    if (lowercaseRes === computer) {
+        return "It's a tie";
+    } else if (lowercaseRes === "rock" && computer === "scissors") {
+        return "you won!";
+    } else if (lowercaseRes === "paper" && computer === "rock") {
+        return "you won!";
+    } else if (lowercaseRes === "scissors" && computer === "paper") {
+        return "you won!";
+    } else {
+        return "you loose!"
+    }
+
+}
+
